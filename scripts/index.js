41d710c502d69331e72f1b375b14ac26006407c1
Martin6000 <martinprado4022@gmail.com>
2025-12-23 11:54:37 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
-c
+const barra = document.getElementById("barra-progreso");
+
+function actualizarBarra() {
+    const scrollTop = window.scrollY;
+    const alturaTotal = document.body.scrollHeight - window.innerHeight;
+    const porcentaje = (scrollTop / alturaTotal) * 100;
+    barra.style.width = porcentaje + "%";
+}
+
+window.addEventListener("scroll", actualizarBarra);
